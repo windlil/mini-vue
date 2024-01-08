0a44ba4102285f58c463a2c7c92a19213d9da132
windlil <spacesouls@163.com>
2024-01-08 23:19:32 +0800

    chore: add readonly fn

--- src/reactivity/reative.ts
+++ src/reactivity/reative.ts
@@ -1,20 +1,37 @@
 import { track, trigger } from "./effect"
 
+function createGetter(isReadonly?) {
+  return function(target, key) {
+    const result = Reflect.get(target, key)
+    if (!isReadonly) {
+      track(target, key)
+    }
+    return result
+  }
+}
+
+function createSetter(isReadonly?) {
+  return function(target, key, value) {
+    if (isReadonly) {
+      console.warn('readonly object cant be modify')
+      return false
+    }
+    const result = Reflect.set(target, key, value)
+    trigger(target, key)
+    return result
+  }
+}
+
 export function reactive(target) {
   return new Proxy(target, {
-    get(target, key) {
-      const value = Reflect.get(target, key)
+    get: createGetter(),
+    set: createSetter(),
+  })
+}
 
-      track(target, key)
-
-      return value
-    },
-    set(target, key, value) {
-      const result = Reflect.set(target, key, value)
-
-      trigger(target, key)
-
-      return result
-    }
+export function readonly(target) {
+  return new Proxy(target, {
+    get: createGetter(true),
+    set: createSetter(true),
   })
 }
